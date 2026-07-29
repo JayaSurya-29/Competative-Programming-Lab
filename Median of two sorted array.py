3baba4a9cfefe83f31f4dce7a70835c052482942
Jaya Surya Paravada <jayasuryaparavada@gmail.com>
2026-07-29 11:26:14 +0530

    Update Median of two sorted array.py

--- Median of two sorted array.py
+++ Median of two sorted array.py
@@ -4,7 +4,7 @@ a2=list(map(int,input().split()))
 a=sorted(a1+a2)
 length=len(a)
 if length%2==1:
-    print(a[length//2])
+    print(f"{float(a[length//2]):.1f}")
 else:
     median=(a[length//2-1]+a[length//2])/2
     print(f"{median:.1f}")
